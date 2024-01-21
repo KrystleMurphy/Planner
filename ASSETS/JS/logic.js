2b063a825f3000cee4f97a14fbd41587dda6c1ca
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-01-21 18:43:17 +0000

    setting up the container with row div-textarea-button x 9

--- ASSETS/JS/logic.js
+++ ASSETS/JS/logic.js
@@ -1,17 +1,15 @@
 
-// convienience var to pull todays date
-var today = dayjs();
+// dayjs().hour()
 // conveinience var for the current day element
 var currentDayEL = $('#currentDay');
 
-var timeEL = $('.time')
 
 // code to display on site
 $(document).ready(function () {
     // code to print formatted date into the current day element
-    currentDayEL.text(today.format("dddd, MMMM Do"));
+    currentDayEL.text(dayjs().format("dddd, MMMM D"));
     // console log to check today var and format
-    console.log(today.format("dddd, MMMM Do"));
+    console.log(dayjs().format("dddd, MMMM D"));
 
 
 
