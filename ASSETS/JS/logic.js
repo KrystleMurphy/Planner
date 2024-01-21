9622a2fefac1addd8e37cf8033d2c744a98029fb
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-01-21 17:41:14 +0000

    adding the save logo

--- ASSETS/JS/logic.js
+++ ASSETS/JS/logic.js
@@ -1,10 +1,22 @@
-$(document).ready(function() {
-    // Get the current date
-    var currentDate = dayjs().format('dddd-MMMM-DD');
-  
-    // Append the formatted date to the element
-    $("#currentDay").text(currentDate);
-  });
+
+// convienience var to pull todays date
+var today = dayjs();
+// conveinience var for the current day element
+var currentDayEL = $('#currentDay');
+
+var timeEL = $('.time')
+
+// code to display on site
+$(document).ready(function () {
+    // code to print formatted date into the current day element
+    currentDayEL.text(today.format("dddd, MMMM Do"));
+    // console log to check today var and format
+    console.log(today.format("dddd, MMMM Do"));
 
 
-  
+
+
+
+
+
+});
